0fe3fedbf250f9cacba9bdcf783320fddb816c78
Sam-Dz-DevOps <141672095+Sam-Dz-Devops@users.noreply.github.com>
2025-06-11 12:38:50 +0000

    update scripts

--- scripts/tf1_m3u8.py
+++ scripts/tf1_m3u8.py
@@ -2,14 +2,14 @@ import requests
 import re
 import os
 
-# URL du fichier .m3u8
+# URL du fichier .m3u8 source
 url = "https://raw.githubusercontent.com/Paradise-91/ParaTV/main/streams/tf1plus/tf1.m3u8"
 
 # Chemin du fichier de sortie
 FHD_OUTPUT_DIR = "./Streams"
 FHD_OUTPUT_FILE = "tf1.m3u8"
 
-# Récupérer le contenu
+# Récupérer le contenu distant
 response = requests.get(url)
 
 # Vérifier le succès de la requête
@@ -22,28 +22,23 @@ if response.status_code == 200:
     urls_sans_suffixe = [re.sub(r'1\.m3u8$', '', url) for url in urls_m3u8]
 
     if urls_sans_suffixe:
-        print("URLs sans le suffixe '1.m3u8' :")
-        for m3u8_url in urls_sans_suffixe:
-            print(m3u8_url)
+        m3u8_url = urls_sans_suffixe[0]  # On prend le premier
+        print(f"URL sans le suffixe '1.m3u8' : {m3u8_url}")
+
+        def generate_m3u_content(m3u8_url, resolution, bandwidth, video_url, audio_url):
+            content = "#EXTM3U\n"
+            content += f"""#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-128",LANGUAGE="fr",NAME="Français",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{m3u8_url}{audio_url}"\n"""
+            content += f"""#EXT-X-STREAM-INF:BANDWIDTH={bandwidth},AVERAGE-BANDWIDTH={bandwidth-200000},CODECS="avc1.64002A,mp4a.40.2",RESOLUTION={resolution},FRAME-RATE=25.000,AUDIO="audio-AACL-128"\n{m3u8_url}{video_url}\n"""
+            return content
+
+        fhd_content = generate_m3u_content(m3u8_url, "1280x720", 4277000, "1.m3u8", "12_0.m3u8")
+
+        os.makedirs(FHD_OUTPUT_DIR, exist_ok=True)
+
+        with open(os.path.join(FHD_OUTPUT_DIR, FHD_OUTPUT_FILE), "w") as f:
+            f.write(fhd_content)
+        print(f"✅ TF1 généré : {FHD_OUTPUT_FILE}")
     else:
         print("Aucune URL contenant '1.m3u8' trouvée.")
 else:
     print(f"Erreur lors de la récupération du fichier: {response.status_code}")
-
-# Contenu des fichiers M3U8
-def generate_m3u_content(m3u8_url, resolution, bandwidth, video_url, audio_url):
-    content = "#EXTM3U\n"
-    if m3u8_url:
-        content += f"""#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-AACL-128",LANGUAGE="fr",NAME="Français",DEFAULT=YES,AUTOSELECT=YES,CHANNELS="2",URI="{m3u8_url}{audio_url}"\n"""
-        content += f"""#EXT-X-STREAM-INF:BANDWIDTH={bandwidth},AVERAGE-BANDWIDTH={bandwidth-200000},CODECS="avc1.64002A,mp4a.40.2",RESOLUTION={resolution},FRAME-RATE=25.000,AUDIO="audio-AACL-128"\n{m3u8_url}{video_url}\n"""
-    return content
-
-fhd_content = generate_m3u_content(m3u8_url, "1280x720", 4277000, "1.m3u8", "12_0.m3u8")
-
-# Création des dossiers
-os.makedirs(FHD_OUTPUT_DIR, exist_ok=True)
-
-# Écriture des fichiers
-with open(os.path.join(FHD_OUTPUT_DIR, FHD_OUTPUT_FILE), "w") as f:
-    f.write(fhd_content)
-print(f"✅ TF1 généré : {FHD_OUTPUT_FILE}")
